a551a5766dfd1bf7fe18e0c0ebaa17e285654fbb
Thomas <jmagtibay39@gmail.com>
2025-11-09 23:01:46 +0800

    Turned Profile to Person

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -48,7 +48,7 @@ class _FirstPageState extends State<FirstPage> {
             //profile
             BottomNavigationBarItem(
               icon: Icon(Icons.person),
-              label: 'Profil',
+              label: 'Person',
             ),
             //settings
             BottomNavigationBarItem(
